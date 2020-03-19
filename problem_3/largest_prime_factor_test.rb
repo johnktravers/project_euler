4c004b54addded5ea327cce2b1c89bed046ecee1
John Travers <johnk.travers@gmail.com>
2020-03-18 21:57:16 -0600

    Refactor prime and largest prime factor methods using prime number logic

--- problem_3/largest_prime_factor_test.rb
+++ problem_3/largest_prime_factor_test.rb
@@ -3,20 +3,29 @@ require 'minitest/pride'
 
 class LargestPrimeFactorTest < Minitest::Test
   def prime(num)
-    return false if num < 2
-    return true if num == 2
-    (3..(num / 2)).step(2).each { |n| return false if num % n == 0 }
-    true
+    if num <= 1
+      return false
+    else
+      (2..(num / 2)).each { |n| return false if num % n == 0 }
+      true
+    end
   end
 
   def largest_prime_factor(num)
-    (num / 2).step(1, -2).each { |n| return n if num % n == 0 && prime(n) }
+    start = num
+    until start.odd?
+      start = start / 2
+    end
+
+    sqrt_max = Math.sqrt(start).to_i + 1
+    sqrt_max.step(3, -2).each { |n| return n if num % n == 0 && prime(n) }
   end
 
   def test_prime
     assert_equal false, prime(0)
     assert_equal false, prime(1)
     assert_equal true, prime(2)
+    assert_equal false, prime(4)
     assert_equal true, prime(5)
     assert_equal true, prime(29)
     assert_equal false, prime(99)
@@ -24,7 +33,6 @@ class LargestPrimeFactorTest < Minitest::Test
 
   def test_largest_prime_factor
     assert_equal 29, largest_prime_factor(13195)
-    # assert_equal 3299, largest_prime_factor(13196)
-    # assert_equal 3299, largest_prime_factor(600851475143)
+    assert_equal 6857, largest_prime_factor(600851475143)
   end
 end
