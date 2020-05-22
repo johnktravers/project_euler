184656f4666253b1c8932885ab41fc10b67d9b14
John Travers <johnk.travers@gmail.com>
2020-05-22 00:03:39 -0400

    Add first month sundays method and test using first days of the months

--- problem_19/counting_sundays_test.rb
+++ problem_19/counting_sundays_test.rb
@@ -3,7 +3,21 @@ require 'minitest/pride'
 
 class CountingSundaysTest < Minitest::Test
   def first_month_sundays(start_year, end_year)
+    count = 0
 
+    (start_year..end_year).each do |year|
+      day = first_sunday(year)
+      until day > 336
+        if leap_year?(year)
+          count += 1 if LEAP_FIRSTS_OF_THE_MONTH.include?(day)
+        else
+          count += 1 if NON_LEAP_FIRSTS_OF_THE_MONTH.include?(day)
+        end
+        day += 7
+      end
+    end
+
+    count
   end
 
   def first_sunday(year)
@@ -36,8 +50,9 @@ class CountingSundaysTest < Minitest::Test
 
   #----------------- Tests -----------------#
 
-  def test_power_digit_sum
-    assert_equal 26, first_month_sundays(1901, 2000)
+  def test_first_month_sundays
+    assert_equal 3,   first_month_sundays(1901, 1902)
+    assert_equal 171, first_month_sundays(1901, 2000)
   end
 
   def test_first_sunday
@@ -53,4 +68,9 @@ class CountingSundaysTest < Minitest::Test
     assert_equal false, leap_year?(1900)
     assert_equal false, leap_year?(1997)
   end
+
+  #----------------- Constants -----------------#
+
+  NON_LEAP_FIRSTS_OF_THE_MONTH = [1, 32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
+  LEAP_FIRSTS_OF_THE_MONTH =     [1, 32, 61, 92, 122, 153, 183, 214, 245, 275, 306, 336]
 end
