8bc9792b04221000c7b81357488f33b143f435ab
John Travers <johnk.travers@gmail.com>
2020-05-27 23:38:29 -0400

    Remove unnecessary amicable pair method and test

--- problem_21/amicable_numbers_test.rb
+++ problem_21/amicable_numbers_test.rb
@@ -4,10 +4,7 @@ require 'minitest/autorun'
 class AmicableNumbersTest < Minitest::Test
   def amicable_number_sum(max_num)
 
-  end
 
-  def amicable_pair(n_1, n_2)
-    n_1 != n_2 && divisor_sum(n_1) == n_2 && divisor_sum(n_2) == n_1
   end
 
   def divisor_sum(num)
@@ -18,11 +15,6 @@ class AmicableNumbersTest < Minitest::Test
 
   def test_amicable_number_sum
     # assert_equal 23, amicable_number_sum(100)
-  end
-
-  def test_amicable_numbers
-    assert_equal true,  amicable_pair(220, 284)
-    assert_equal false, amicable_pair(100, 100)
   end
 
   def test_divisor_sum
