fdaf30d0d6fc81c804cdbfacc03288e6ffe81cf5
John Travers <johnk.travers@gmail.com>
2020-06-10 22:35:08 -0600

    Add sum of two abundants method and test

--- problem_23/nonabundant_sums_test.rb
+++ problem_23/nonabundant_sums_test.rb
@@ -3,7 +3,18 @@ require 'minitest/autorun'
 
 class NonabundantSumsTest < Minitest::Test
   def nonabundant_sums(max_num)
+  end
 
+  def sum_of_two_abundants?(num, abundants)
+    remainders = {}
+
+    abundants.each do |a|
+      remainder = num - a
+      return true if remainders[a] || remainder == a
+      remainders[remainder] = a
+    end
+
+    false
   end
 
   def abundant?(num)
@@ -23,6 +34,14 @@ class NonabundantSumsTest < Minitest::Test
     skip
     assert_equal 504,   nonabundant_sums(1000)
     assert_equal 31626, nonabundant_sums(10000)
+  end
+
+  def test_sum_of_two_abundants
+    assert_equal false, sum_of_two_abundants?(12, [])
+    assert_equal true,  sum_of_two_abundants?(24, [12, 18, 20])
+
+    abundants = [12, 18, 20, 24, 30, 36, 40, 42, 48, 54, 56, 60, 66, 70, 72]
+    assert_equal true,  sum_of_two_abundants?(78, abundants)
   end
 
   def test_abundant
