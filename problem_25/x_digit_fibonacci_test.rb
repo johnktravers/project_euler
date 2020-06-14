43572905af5faa738588a0f33eb98980b9dd5fd2
John Travers <johnk.travers@gmail.com>
2020-06-13 22:30:06 -0600

    Add test for fibonacci index method

--- problem_25/x_digit_fibonacci_test.rb
+++ problem_25/x_digit_fibonacci_test.rb
@@ -3,12 +3,22 @@ require 'minitest/autorun'
 
 class XDigitFibonacciTest < Minitest::Test
   def x_digit_fibonacci(x)
-    
+
+  end
+
+  def fibonacci_index(num)
+
   end
 
   #----------------- Tests -----------------#
 
   def test_x_digit_fibonacci
     assert_equal 144, x_digit_fibonacci(3)
+  end
+
+  def test_fibonacci_index
+    assert_equal 3,  fibonacci_index(2)
+    assert_equal 9,  fibonacci_index(34)
+    assert_equal 12, fibonacci_index(144)
   end
 end
