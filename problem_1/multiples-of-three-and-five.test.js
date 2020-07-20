579b6e5e0fdcadde1564451098a31e7bd59ee7de
John Travers <johnk.travers@gmail.com>
2020-07-19 21:38:57 -0600

    Add javascript solution to multiples of three and five problem

---
+++ problem_1/multiples-of-three-and-five.test.js
@@ -0,0 +1,15 @@
+function multiplesOfThreeAndFive(max_num) {
+  let sum = 0;
+  for (const n of Array(max_num).keys()) {
+    if (n % 3 === 0 || n % 5 === 0) {
+      sum += n;
+    }
+  }
+  return sum;
+}
+
+test('multiplesOfThreeAndFive', () => {
+  expect(multiplesOfThreeAndFive(10)).toBe(23);
+  expect(multiplesOfThreeAndFive(100)).toBe(2318);
+  expect(multiplesOfThreeAndFive(1000)).toBe(233168);
+});
