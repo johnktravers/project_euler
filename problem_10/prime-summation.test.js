0b5a52f893e7fd45fb4a3fa6afde8ec9f26cc25c
John Travers <johnk.travers@gmail.com>
2020-08-08 23:45:23 -0600

    Add javascript prime summation function with tests

--- problem_10/prime-summation.test.js
+++ problem_10/prime-summation.test.js
@@ -1,3 +1,14 @@
+function primeSummation(maxNum) {
+  let sum = 0;
+  for (n = 2; n < maxNum; n++) {
+    if (prime(n)) {
+      sum += n;
+    }
+  }
+
+  return sum;
+}
+
 function prime(num) {
   if (num < 2) {
     return false;
@@ -13,6 +24,12 @@ function prime(num) {
 }
 
 //----------------- Tests -----------------//
+
+test("primeSummation", () => {
+  expect(primeSummation(10)).toBe(17);
+  expect(primeSummation(1000)).toBe(76127);
+  expect(primeSummation(2000000)).toBe(142913828922);
+});
 
 test("prime", () => {
   expect(prime(0)).toBe(false);
